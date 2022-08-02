54aa14f39fd3a55309c04b6a69b70d16ed8a9d31
Tot Truong <tat.tran@gmx.net>
2022-08-03 01:28:11 +0700

    - first batch

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -6,6 +6,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move { x: u32, y: u32 },
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
 }
 
 struct Point {
@@ -38,6 +42,12 @@ impl State {
 
     fn process(&mut self, message: Message) {
         // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor => println!("ChangeColor"),
+            Message::Echo => println!("Echo"),
+            Message::Move => println!("Move"),
+            Message::Quit => println!("Quit"),
+        }    
     }
 }
 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -8,10 +8,15 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amount :u32) -> u32 {
+    if amount <= 40 {
+        amount * 2
+    } else {
+        amount
+    }
+}
 
 // Don't modify this function!
 #[test]
